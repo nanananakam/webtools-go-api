8ae350a68a462d70501e58569e7f02863442a28e
nanananakam <naoto@nanananakam.com>
2022-11-06 15:18:31 +0900

    GeoIP2Lite対応追加

--- whois/whois_test.go
+++ whois/whois_test.go
@@ -9,15 +9,17 @@ import (
 
 // response from APNIC
 func TestIP1_1_1_1(t *testing.T) {
-	_, err := getIp2Location("1.1.1.1")
-	if err != nil {
-		panic(err)
+	if _, err := getIp2Location("1.1.1.1"); err != nil {
+		t.Error(err)
+	}
+	if _, err := getGeoIp2Location("1.1.1.1"); err != nil {
+		t.Error(err)
 	}
 
 	dir, _ := os.Getwd()
 	rdapResponseRawString, err := os.ReadFile(dir + "/rdapRawResponse1.1.1.1.json")
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
 
 	var rdapResponse rdapResponse
@@ -41,15 +43,17 @@ func TestIP1_1_1_1(t *testing.T) {
 
 // response from ARIN
 func TestIP8_8_8_8(t *testing.T) {
-	_, err := getIp2Location("8.8.8.8")
-	if err != nil {
-		panic(err)
+	if _, err := getIp2Location("8.8.8.8"); err != nil {
+		t.Error(err)
+	}
+	if _, err := getGeoIp2Location("8.8.8.8"); err != nil {
+		t.Error(err)
 	}
 
 	dir, _ := os.Getwd()
 	rdapResponseRawString, err := os.ReadFile(dir + "/rdapRawResponse8.8.8.8.json")
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
 
 	var rdapResponse rdapResponse
@@ -68,15 +72,17 @@ func TestIP8_8_8_8(t *testing.T) {
 
 // response from RIPE
 func TestIP2_2_2_2(t *testing.T) {
-	_, err := getIp2Location("2.2.2.2")
-	if err != nil {
-		panic(err)
+	if _, err := getIp2Location("2.2.2.2"); err != nil {
+		t.Error(err)
+	}
+	if _, err := getGeoIp2Location("2.2.2.2"); err != nil {
+		t.Error(err)
 	}
 
 	dir, _ := os.Getwd()
 	rdapResponseRawString, err := os.ReadFile(dir + "/rdapRawResponse2.2.2.2.json")
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
 
 	var rdapResponse rdapResponse
@@ -96,15 +102,17 @@ func TestIP2_2_2_2(t *testing.T) {
 // response from AFRINIC
 // AFRINICは担当者個人名？が取れてしまうが組織名らしき文字列は無いしそういうものらしい
 func TestIP196_46_23_70(t *testing.T) {
-	_, err := getIp2Location("196.46.23.70")
-	if err != nil {
-		panic(err)
+	if _, err := getIp2Location("196.46.23.70"); err != nil {
+		t.Error(err)
+	}
+	if _, err := getGeoIp2Location("196.46.23.70"); err != nil {
+		t.Error(err)
 	}
 
 	dir, _ := os.Getwd()
 	rdapResponseRawString, err := os.ReadFile(dir + "/rdapRawResponse196.46.23.70.json")
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
 
 	var rdapResponse rdapResponse
@@ -123,15 +131,17 @@ func TestIP196_46_23_70(t *testing.T) {
 
 // response from LACNIC
 func TestIp190_120_4_21(t *testing.T) {
-	_, err := getIp2Location("190.120.4.21")
-	if err != nil {
-		panic(err)
+	if _, err := getIp2Location("190.120.4.21"); err != nil {
+		t.Error(err)
+	}
+	if _, err := getGeoIp2Location("190.120.4.21"); err != nil {
+		t.Error(err)
 	}
 
 	dir, _ := os.Getwd()
 	rdapResponseRawString, err := os.ReadFile(dir + "/rdapRawResponse190.120.4.21.json")
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
 
 	var rdapResponse rdapResponse
@@ -150,8 +160,10 @@ func TestIp190_120_4_21(t *testing.T) {
 
 // rdap以外でIPv6がエラーにならないか確認
 func TestIpV6(t *testing.T) {
-	_, err := getIp2Location(" 2606:4700:4700::1111")
-	if err != nil {
-		panic(err)
+	if _, err := getIp2Location("2606:4700:4700::1111"); err != nil {
+		t.Error(err)
+	}
+	if _, err := getGeoIp2Location("2606:4700:4700::1111"); err != nil {
+		t.Error(err)
 	}
 }
